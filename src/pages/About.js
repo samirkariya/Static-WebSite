aa435261a5687e3d1feda66035defd0f223601a9
samirkariya <samir.kariya@darshan.ac.in>
2025-08-30 13:09:12 +0530

    Update About.js
    Name Changed in About File.

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ function About() {
                 <div class="container">
                     <h1 class="display-5">About us</h1>
                     <p style={{fontSize:21}}>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-                    <p><a class="btn btn-primary btn-sm" href="#" role="button">RP &raquo;</a></p>
+                    <p><a class="btn btn-primary btn-sm" href="#" role="button">Samir &raquo;</a></p>
                 </div>
             </div>
         </main>
