b412a1522a0d7ef3a6a9adc1b5c25ffe3ec5e562
samirkariya <samir.kariya@darshan.ac.in>
2025-08-30 13:08:40 +0530

    Update Contact.js
    Name Changed

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
                 <div class="container">
                     <h1 class="display-5">Contact Us</h1>
                     <p style={{fontSize:21}}>If you have any requirement about Develop website and Site Reliability and Many other things Fill this From Given Below My Team Will Contact You Shortly.</p>
-                    <p><a class="btn btn-primary btn-sm" href="#" role="button">RP &raquo;</a></p>
+                    <p><a class="btn btn-primary btn-sm" href="#" role="button">Samir &raquo;</a></p>
                 </div>
             </div>
         </main>
