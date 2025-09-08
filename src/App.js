b515a79e3dc70f6dfd84a4979067eae2b5b8b373
samirkariya <samir.kariya@darshan.ac.in>
2025-09-08 12:08:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     return <>
         <Router>
             <nav class="navbar navbar-expand-lg navbar-light bg-light navbar navbar-dark bg-dark">
-                <Link to="/" class="navbar-brand">Samir</Link>
+                <Link to="/" class="navbar-brand">SamirKariya</Link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
